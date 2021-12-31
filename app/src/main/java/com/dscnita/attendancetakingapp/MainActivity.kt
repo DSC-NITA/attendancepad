2d48635de10d41b7d01559986d82f483771c45b2
Akshat Kumar Verma <akshatvermajbp@gmail.com>
2021-12-31 13:46:06 +0530

    Added FAB (#4)

--- app/src/main/java/com/dscnita/attendancetakingapp/MainActivity.kt
+++ app/src/main/java/com/dscnita/attendancetakingapp/MainActivity.kt
@@ -2,12 +2,29 @@ package com.dscnita.attendancetakingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import com.dscnita.attendancetakingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabMain.setOnClickListener {
+            showDialogBox()
+        }
+    }
+
+    private fun showDialogBox()
+    {
+        val builder=AlertDialog.Builder(this)
+        val view=LayoutInflater.from(this).inflate(R.layout.class_dialogue,null)
+        builder.setView(view)
+        builder.create().show()
     }
 }
